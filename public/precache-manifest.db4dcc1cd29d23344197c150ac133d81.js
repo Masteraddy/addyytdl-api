305c92707fc6569205e9356415341535920bf776
Green Sostein <greenfacilityltd@gmail.com>
2020-06-02 03:53:59 +0100

    More Commit

---
+++ public/precache-manifest.db4dcc1cd29d23344197c150ac133d81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9638fb5cd867a77d5d816d448824ed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc8debda630cfeb41612",
+    "url": "/static/css/main.884fccee.chunk.css"
+  },
+  {
+    "revision": "7c70197755451ef85292",
+    "url": "/static/js/2.799c6176.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.799c6176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc8debda630cfeb41612",
+    "url": "/static/js/main.4d315b26.chunk.js"
+  },
+  {
+    "revision": "8368dbc3b6c94edf4c6e",
+    "url": "/static/js/runtime-main.9f563737.js"
+  }
+]);
